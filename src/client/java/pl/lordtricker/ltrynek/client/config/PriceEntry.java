ed55164920f9b9f27d72fa89265d5393a7a11fa0
SzybkiMaksiu <noobmarszalkowski@gmail.com>
2026-01-19 22:41:04 +0100

    Delete src/client directory

--- src/client/java/pl/lordtricker/ltrynek/client/config/PriceEntry.java
+++
@@ -1,9 +0,0 @@
-package pl.lordtricker.ltrynek.client.config;
-
-public class PriceEntry {
-    public String name;
-    public String lore;
-    public String material;
-    public double maxPrice;
-    public String enchants;
-}
